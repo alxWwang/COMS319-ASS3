2d68af2dc832bda069e4b6b5722fbf43ce5e5e95
Yi Yun Khor <annabelle@Yis-MacBook-Air.local>
2024-04-25 13:02:08 -0500

    testing if annabelle is in

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>IM HERE</h1>
     <App />
   </React.StrictMode>
 );
