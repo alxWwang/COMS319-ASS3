ad9c87068364d0fa7f4f9b4beddce35b5b492001
Annabelle5474 <15776ABk@gmail.com>
2024-04-27 12:43:29 -0500

    upload 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { useForm } from "react-hook-form";
 import nickpic from "/Users/annabelle/Downloads/ass3MAIN/COMS319-ASS3/frontend/src/Images/gsnteng.jpg";
 import annapic from "/Users/annabelle/Downloads/ass3MAIN/COMS319-ASS3/frontend/src/Images/anabelle.jpeg"
